28a8ec738ad4431fcb1953feb198aa6fe9b21017
derrick b <dbozich1@gmail.com>
2018-05-31 06:20:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,5 +4,23 @@ class Song
   def initialize(title)
     @title = title
   end
-  
+
 end
+
+class Artist
+  attr_accessor :name, :genre, :song
+
+  def initialize(name, genre)
+    @name = name
+    @genre = genre
+  end
+
+end
+
+# head_over_heels = Song.new("Head Over Heels")
+# tears_for_fears = Artist.new("Tears For Fears", "Synth-Pop")
+# head_over_heels.artist = tears_for_fears
+#
+# tears_for_fears.song = head_over_heels
+#
+# tears_for_fears.song.title
