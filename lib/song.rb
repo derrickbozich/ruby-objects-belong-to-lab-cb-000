11f71d690528c815b26716282071ba83055d5cf4
derrick b <dbozich1@gmail.com>
2018-05-31 06:05:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,22 +3,6 @@ class Song
 
   def initialize(title)
     @title = title
-    
-
   end
+  
 end
-
-class Artist
-  attr_accessor :name, :genre
-
-  def initialize(name, genre)
-    @name = name
-    @genre = genre
-  end
-
-end
-
-drake = Artist.new("Drake")
-
-hotline_bling = Song.new("Hotline Bling")
-hotline_bling.artist = drake
